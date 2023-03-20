bd5bc18480243fec8de1b86a8494202e5dbbe13a
dsspsoft <daniil.sarzhan@spsoft.com>
2023-03-20 22:31:21 +0100

    Push project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input file name");
+        String fileName = scanner.nextLine();
+        scanner.close();
+
+        String directoryPath = "/Users/daniil_admin/Desktop/newfolder/"; // path directory with files for rename
+        int fileNumber = 1; // first number file
+
+        File directory = new File(directoryPath);
+        File[] filesInDirectory = directory.listFiles();
+        for (File videoFile : filesInDirectory) {
+            if (!videoFile.isDirectory()) {
+                String fileExtension = getFileExtension(videoFile.getName());
+
+                String newFileName = fileName + "_" + fileNumber + fileExtension;
+                File newFile = new File(directoryPath + newFileName);
+
+                boolean isRenamed = videoFile.renameTo(newFile);
+                if (isRenamed) {
+                    System.out.println("File " + videoFile.getName() + " successfully rename at " + newFileName);
+                    fileNumber++;
+                } else {
+                    System.out.println("Failed to rename file " + videoFile.getName());
+                }
+            }
+        }
+    }
+
+    private static String getFileExtension(String fileName) {
+        int dotIndex = fileName.lastIndexOf(".");
+        if (dotIndex == -1) {
+            return "";
+        }
+        return fileName.substring(dotIndex);
+    }
+}
